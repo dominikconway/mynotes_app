f3019c8a0f21e0e3ad6cc25ade9bdd325ac1a57e
Dominik Conway <dominik.conway@gmail.com>
2022-08-10 22:51:14 -0700

    adds note and note list and styles the note

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import NotesList from "./components/NotesList"
+import { useState } from "react"
+import { nanoid } from 'nanoid'
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+    id: nanoid(),
+    text: 'This is my first note!',
+    date: '28/04/2021'
+  },
+  {
+    id: nanoid(),
+    text: 'This is my second note!',
+    date: '12/04/2021'
+  },
+  {
+    id: nanoid(),
+    text: 'This is my third note!',
+    date: '2/04/2021'
+  },
+  {
+    id: nanoid(),
+    text: 'This is my fourth note!',
+    date: '2/04/2021'
+  }
+])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container">
+      <NotesList notes={notes} />
+   
+     </div>
+    )
 }
 
-export default App;
+export default App
